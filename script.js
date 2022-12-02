bee79bcbb6cd751e0147731e8aa6d47881fb19ae
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2022-12-01 21:09:36 -0300

    Finalizando projeto

--- script.js
+++ script.js
@@ -9,20 +9,33 @@ const buttonRemoveCinco = document.querySelector('.removeCinco');
 const secondsDisplay = document.querySelector('.seconds')
 const minutesDisplay = document.querySelector('.minutes')
 
-// variaveis dos buttoes do son 
+// variaveis dos buttoes do som 
 const buttonFloresta = document.querySelector('.floresta')
+const buttonChuva = document.querySelector('.chuva')
+const buttonCafeteria = document.querySelector('.cafeteria')
+const buttonLareira = document.querySelector('.lareira')
 
+// variaveis de audio
+const audioFloresta = new Audio("audios/Floresta.wav")
+const audioChuva = new Audio("audios/Chuva.wav")
+const audioCafeteria = new Audio("audios/Cafeteria.wav")
+const audioLareira = new Audio("audios/Lareira.wav")
+
+//eventos dos butões do timer
 buttonPlay.addEventListener('click', playTimer)
 buttonPause.addEventListener('click', pause)
 buttonStop.addEventListener('click', stop)
-buttonStop.addEventListener('click', function(){
-    clearTimeout(timerTimeOut)
-})
-
+buttonStop.addEventListener('click', function(){clearTimeout(timerTimeOut)})
 buttonAddCinco.addEventListener('click', maisCinco)
 buttonRemoveCinco.addEventListener('click', menosCinco)
 
+
+//eventos dos buttoes do som 
 buttonFloresta.addEventListener('click', floresta)
+buttonChuva.addEventListener('click', chuva)
+buttonCafeteria.addEventListener('click', cafeteria)
+buttonChuva.addEventListener('click', chuva)
+buttonLareira.addEventListener('click', lareira)
 
 function playTimer(){  
     buttonPlay.classList.add('hide')
@@ -82,9 +95,54 @@ function menosCinco() {
 }
 
 function floresta() {
-    const audioFloresta = new Audio("audios/Floresta.wav?raw=true")
     audioFloresta.play()
+    audioChuva.pause()
+    audioLareira.pause()
+    audioCafeteria.pause() 
+    
+    buttonFloresta.classList.add("hidee")
+    buttonChuva.classList.remove("hidee")
+    buttonCafeteria.classList.remove("hidee")
+    buttonLareira.classList.remove("hidee")
 }
+
+function chuva() {
+    audioChuva.play()
+    audioFloresta.pause()
+    audioLareira.pause()
+    audioCafeteria.pause()
+    
+    buttonFloresta.classList.remove("hidee")
+    buttonChuva.classList.add("hidee")
+    buttonCafeteria.classList.remove("hidee")
+    buttonLareira.classList.remove("hidee")
+}
+
+function cafeteria() {
+    audioFloresta.pause()
+    audioChuva.pause()
+    audioLareira.pause()
+    audioCafeteria.play() 
+    
+    buttonFloresta.classList.remove("hidee")
+    buttonChuva.classList.remove("hidee")
+    buttonCafeteria.classList.add("hidee")
+    buttonLareira.classList.remove("hidee")
+}
+
+function lareira() {
+    audioLareira.play()
+    audioFloresta.pause()
+    audioChuva.pause()
+    audioCafeteria.pause()
+    
+    buttonFloresta.classList.remove("hidee")
+    buttonChuva.classList.remove("hidee")
+    buttonCafeteria.classList.remove("hidee")
+    buttonLareira.classList.add("hidee")
+    
+}
+
 
 
 
